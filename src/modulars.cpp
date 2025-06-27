ad621e0e91ec593527d453f37d40227e2a741fdd
liwei-jpg <leeliwei0906@hotmail.com>
2025-06-27 22:47:59 +0800

    To select one or two players

--- src/modulars.cpp
+++ src/modulars.cpp
@@ -57,8 +57,36 @@ public:
 };
 
 void runGame() {
+    int mode;
+    string player1, player2;
+
+    cout << "\033[2J\033[1;1H"; // Clear screen (ANSI escape code for VS Code)
     displayTitle();
-    cout << "Game started...\n";
+    cout << "Welcome to Hangman! Please select your mode\n\n";
+    cout << "[1] Single player   (vs. computer)\n";
+    cout << "[2] Two player      (vs. each other)\n\n";
+    cout << "Select mode number : ";
+    cin >> mode;
+
+    cout << "\033[2J\033[1;1H"; // Clear screen again
+    displayTitle();
+    cout << "====================  M O D E   S E L E C T E D  :  " << mode << "  =====================\n\n";
+
+    if (mode == 1) {
+        cout << "Please enter your name: ";
+        cin >> player1;
+        cout << "\nGood luck in your game later, player " << player1 << "!\n";
+    } else if (mode == 2) {
+        cout << "Please enter your name\n";
+        cout << "Player 1 : ";
+        cin >> player1;
+        cout << "Player 2 : ";
+        cin >> player2;
+        cout << "\nGood luck in your game later, player " << player1 << " and player " << player2 << "!\n";
+    } else {
+        cout << "Invalid mode. Exiting...\n";
+        return;
+    }
+
     waitForKey();
-    displayExitMessage();
 }
